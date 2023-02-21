6c24d53c4f7021e9fe6d27614e51771221ca1a24
Yating Song <yatingsong7@gmail.com>
2023-02-21 23:55:18 +1100

    install material ui and create theme file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React, { FC, ReactElement } from "react";
 import "./App.css";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import customTheme from "./theme/customeTheme";
 
 const App: FC = (): ReactElement => {
-  return <h1>Hello World!</h1>;
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <h1>Hello World!</h1>
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ src/theme/customeTheme.ts
@@ -0,0 +1,9 @@
+import { createTheme, ThemeOptions } from "@mui/material";
+
+const customTheme: ThemeOptions = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default customTheme;
