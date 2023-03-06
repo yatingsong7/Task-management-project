6abd8f32ca7f024c432ccc5c9085871619641441
Yating Song <yatingsong7@gmail.com>
2023-03-06 21:13:59 +1100

    add props to progress indicator area

--- src/components/progress/Progress.tsx
+++ src/components/progress/Progress.tsx
@@ -2,15 +2,24 @@ import { Box } from "@mui/material";
 import React, { FC, ReactElement } from "react";
 import Indicator from "./_Indicator";
 import { INDICATOR_COLOR } from "./interfaces/IIndicator";
+import { IProgress } from "./interfaces/IProgress";
+import PropTypes from "prop-types";
 
-const Progress: FC = (): ReactElement => {
+const Progress: FC<IProgress> = (props): ReactElement => {
+  const { todoCount = 0, inProgressCount = 0, completeCount = 0 } = props;
   return (
-    <Box display="flex" justifyContent="center" alignItems="center">
-      <Indicator color={INDICATOR_COLOR.error} label={"To Do"} count={0} />
-      <Indicator color={INDICATOR_COLOR.primary} label={"In Progress"} count={0} />
-      <Indicator color={INDICATOR_COLOR.success} label={"Completed"} count={0} />
+    <Box display="flex" justifyContent="center" alignItems="center" m={6.6}>
+      <Indicator color={INDICATOR_COLOR.error} label={"To Do"} count={todoCount} />
+      <Indicator color={INDICATOR_COLOR.primary} label={"In Progress"} count={inProgressCount} />
+      <Indicator color={INDICATOR_COLOR.success} label={"Completed"} count={completeCount} />
     </Box>
   );
 };
 
 export default Progress;
+
+Progress.propTypes = {
+  todoCount: PropTypes.number,
+  inProgressCount: PropTypes.number,
+  completeCount: PropTypes.number,
+};
---
+++ src/components/progress/interfaces/IProgress.tsx
@@ -0,0 +1,5 @@
+export interface IProgress {
+  todoCount?: number;
+  inProgressCount?: number;
+  completeCount?: number;
+}
