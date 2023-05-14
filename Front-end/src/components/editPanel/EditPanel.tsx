2a1b41db10022c83be8dff08a88e8e21e22cff01
Yating Song <yatingsong7@gmail.com>
2023-05-14 23:00:04 +1000

    Add task info on edit panel

--- Front-end/src/components/editPanel/EditPanel.tsx
+++ Front-end/src/components/editPanel/EditPanel.tsx
@@ -1,10 +1,21 @@
 import React, { FC, ReactElement, useContext, useEffect, useState } from "react";
 import { EditTaskContext } from "../../context/EditTaskContext";
+import { Box, Typography } from "@mui/material";
 
 const EditPanel: FC = (): ReactElement => {
   const editTaskContext = useContext(EditTaskContext);
-
-  return <div>{editTaskContext.task.id}</div>;
+  console.log(editTaskContext.task.date);
+  return (
+    <Box marginTop={3}>
+      <Typography fontWeight={700} fontSize={30}>
+        {editTaskContext.task.title}
+      </Typography>
+      <Typography>Description: {editTaskContext.task.description}</Typography>
+      {/* <Typography>{editTaskContext.task.date.getTime()}</Typography> */}
+      <Typography>Satus: {editTaskContext.task.status}</Typography>
+      <Typography>Priority: {editTaskContext.task.priority}</Typography>
+    </Box>
+  );
 };
 
 export default EditPanel;
